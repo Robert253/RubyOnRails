ac947c55cc5461099cee5a2d3d81ecc614cd381d
Robert253 <Robert253web@gmail.com>
2021-02-09 15:36:58 +0000

    divise/bootstrap

---
+++ config/webpack/environment.js
@@ -0,0 +1,10 @@
+const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+environment.plugins.append('provide',
+new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['Popper.js', 'default']
+  })
+)
+module.exports = environment
